ca551a39486801c1e4fb5131a81dce122efbceec
Karter Prehn <kd9566gs@go.minnstate.edu>
2023-11-16 15:58:02 -0600

    Added additional InputBox for text boxes
    PIN, Keywords, and ConjectureBox.

--- src/components/ConjectureModule/ConjectureModuleBoxes.js
+++ src/components/ConjectureModule/ConjectureModuleBoxes.js
@@ -35,7 +35,7 @@ export const NameBox = (props) => {
         <Text
         text={"NAME:"}
         x={props.width * 0.102}
-        y={props.height * 0.15}
+        y={props.height * 0.13}
         style={
           new TextStyle({
             align: "center",
@@ -53,7 +53,7 @@ export const NameBox = (props) => {
           height={height * 0.13}
           width={width * 1.05}
           x={width * 0.134}
-          y={height * 0.125}
+          y={height * 0.105}
           color={white}
           fontSize={width * 0.014}
           fontColor={black}
@@ -69,7 +69,7 @@ export const NameBox = (props) => {
         <Text
         text={"AUTHOR:"}
         x={props.width * 0.6}
-        y={props.height * 0.15}
+        y={props.height * 0.13}
         style={
           new TextStyle({
             align: "center",
@@ -120,13 +120,16 @@ export const NameBox = (props) => {
 
 export const PINBox = (props) => {
   const { height, width } = props;
+
   // Creates a popup in which the user can set a pin for their conjecture
   function pinBoxInput() {
-    let pin = prompt("Please Enter Your PIN");
-    if (!isNaN(pin)) {
-      localStorage.setItem('PIN', pin) }
-    else {
-      // Implement error message saying that pin must be numeric
+    const existingPin = localStorage.getItem('PIN');
+    let pin = prompt("Please Enter Your PIN", existingPin);
+
+    if (!isNaN(pin) && pin !== null) {
+      localStorage.setItem('PIN', pin);
+    } else if (pin !== null) {
+      alert('PIN must be numeric');
     }
   }
 
@@ -135,7 +138,7 @@ export const PINBox = (props) => {
       <Text
       text={"PIN:"}
       x={props.width * 0.797}
-      y={props.height * 0.15}
+      y={props.height * 0.13}
       style={
         new TextStyle({
           align: "center",
@@ -148,78 +151,108 @@ export const PINBox = (props) => {
       }
       anchor={0.5}
       />
-        {/* PINBox InputBox */}
-        <InputBox
-          height={height * 0.10}
-          width={width * 0.3}
-          x={width * 0.818}
-          y={height * 0.13}
-          color={white}
-          fontSize={width * 0.015}
-          fontColor={black}
-          text={localStorage.getItem('PIN')}
-          fontWeight={300}
-          callback={pinBoxInput} // Implement Popup
-        />
+      {/* PINBox InputBox */}
+      <InputBox
+        height={height * 0.10}
+        width={width * 0.3}
+        x={width * 0.818}
+        y={height * 0.11}
+        color={white}
+        fontSize={width * 0.015}
+        fontColor={black}
+        text={
+          localStorage.getItem('PIN') || '' // Show existing PIN if available
+        }
+        fontWeight={800}
+        callback={pinBoxInput} // Implement Popup
+      />
       </>
   )
 }
 
 export const ConjectureBox = (props) => {
   const { height, width } = props;
+
+  // Function to handle conjecture input
+  function conjectureBoxInput() {
+    const existingConjecture = localStorage.getItem('Conjecture');
+    const newConjecture = prompt("Please Enter Your Conjecture", existingConjecture);
+
+    if (newConjecture !== null) {
+      localStorage.setItem('Conjecture', newConjecture);
+    }
+  }
+
   return (
-      <>
-        {/* ConjextureBox InputBox */}
-        <InputBox
-          height={height * 0.30}
-          width={width * 2.1645}
-          x={width * 0.072}
-          y={height * 0.1755}
-          color={white}
-          fontSize={width * 0.02}
-          fontColor={black}
-          text={null}
-          fontWeight={800}
-          callback={null} // Implement Popup
-        />
-      </>
+    <>
+      {/* ConjectureBox InputBox */}
+      <InputBox
+        height={height * 0.30}
+        width={width * 2.1645}
+        x={width * 0.072}
+        y={height * 0.1755}
+        color={white}
+        fontSize={width * 0.015}
+        fontColor={black}
+        text={
+          localStorage.getItem('Conjecture')?.slice(0, 200) +
+          (localStorage.getItem('Conjecture')?.length > 200 ? '...' : '')
+        }
+        fontWeight={800}
+        callback={conjectureBoxInput} // Implement Popup for conjecture input
+      />
+    </>
   )
 }
 
 export const KeywordsBox = (props) => {
   const { height, width } = props;
+
+  // Function to handle keywords input
+  function keywordsBoxInput() {
+    const existingKeywords = localStorage.getItem('Keywords');
+    const newKeywords = prompt("Please Enter Keywords (comma-separated)", existingKeywords);
+
+    if (newKeywords !== null) {
+      localStorage.setItem('Keywords', newKeywords);
+    }
+  }
+
   return (
-      <>
-        <Text
-          text={"KEYWORDS:"}
-          x={props.width * 0.1275}
-          y={props.height * 0.322}
-          style={
-            new TextStyle({
-              align: "center",
-              fontFamily: "Futura",
-              fontSize: props.width * 0.018,
-              fontWeight: 800,
-              fill: [blue],
-              letterSpacing: 0,
-            })
-          }
-          anchor={0.5}
-        />
-        {/* KeywordsBox InputBox */}
-        <InputBox
-          height={height * 0.10}
-          width={width * 1.8815}
-          x={width * 0.1855}
-          y={props.height * 0.301}
-          color={white}
-          fontSize={width * 0.02}
-          fontColor={black}
-          text={null}
-          fontWeight={800}
-          outlineColor={black}
-          callback={null} // Implement Popup
-        />
-      </>
+    <>
+      <Text
+        text={"KEYWORDS:"}
+        x={props.width * 0.1275}
+        y={props.height * 0.322}
+        style={
+          new TextStyle({
+            align: "center",
+            fontFamily: "Futura",
+            fontSize: props.width * 0.018,
+            fontWeight: 800,
+            fill: [blue],
+            letterSpacing: 0,
+          })
+        }
+        anchor={0.5}
+      />
+      {/* KeywordsBox InputBox */}
+      <InputBox
+        height={height * 0.10}
+        width={width * 1.8815}
+        x={width * 0.1855}
+        y={height * 0.301}
+        color={white}
+        fontSize={width * 0.015}
+        fontColor={black}
+        text={
+          localStorage.getItem('Keywords')?.slice(0, 100) +
+          (localStorage.getItem('Keywords')?.length > 100 ? '...' : '')
+        }
+        fontWeight={800}
+        outlineColor={black}
+        callback={keywordsBoxInput} // Implement Popup for keywords input
+      />
+    </>
   )
 }
