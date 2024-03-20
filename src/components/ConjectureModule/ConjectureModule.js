b55fc243d46a3e7f3dd93ef938e83d4a4515ebb9
Rania0805 <raniaaweni.anjorin@mnsu.edu>
2024-03-20 11:47:57 -0500

    Update conjecture module.js
    Check if the new conjecture has been saved or published if no when it clicks on the back button a prompt message comes asking if they still want to do it

--- src/components/ConjectureModule/ConjectureModule.js
+++ src/components/ConjectureModule/ConjectureModule.js
@@ -1,4 +1,5 @@
 import Background from "../Background";
+import React, { useState} from 'react';
 import { green, neonGreen, black, blue, white, pink, orange, red, transparent, turquoise } from "../../utils/colors";
 import Button from "../Button";
 import RectButton from "../RectButton";
@@ -40,180 +41,205 @@ function setLocalStorage(){
 const ConjectureModule = (props) => {
   const { height, width, poseData, columnDimensions, rowDimensions, editCallback, mainCallback } = props;
   const [state, send] = useMachine(ConjectureEditorMachine);
+  const [isSaved, setIsSaved] = useState(false);
   setLocalStorage();
 
-    return (
-      <>
-        <Background height={height * 1.1} width={width} />
-        <NameBox height={height} width={width} boxState={state.value} />
-        <PINBox height={height} width={width} />
-        <StartBox height={height * 0.5} width={width * 0.5} x={5} y={4.6} boxState={null} similarityScores={null} inCE={true} />
-        <IntermediateBox height={height * 0.5} width={width * 0.5} x={9} y={1.906} boxState={null} similarityScores={null} inCE={true} />
-        <EndBox height={height * 0.5} width={width * 0.5} x={13} y={1.2035} boxState={null} similarityScores={null} inCE={true} />
-        {/* Button to Pose Editor */}
-        <Button
-          height={height * 0.12}
-          width={width * 0.0950}
-          x={width * 0.17}
-          y={height * 0.42}
-          color={blue}
-          fontSize={21}
-          fontColor={white}
-          text={"POSE EDITOR"}
-          fontWeight={800}
-          callback={editCallback}
-        />
-          <Button
-            height={height * 0.14}
-            width={width * 0.04}
-            x={width * 0.10}
-            y={height * 0.61}
-            color={blue}
-            fontSize={40}
-            fontColor={white}
-            text={"A"}
-            fontWeight={800}
-            callback={null}
-        />
-          <Button
-            height={height * 0.14}
-            width={width * 0.04}
-            x={width * 0.10}
-            y={height * 0.70}
-            color={blue}
-            fontSize={40}
-            fontColor={white}
-            text={"B"}
-            fontWeight={800}
-            callback={null}
-        />
-          <Button
-            height={height * 0.14}
-            width={width * 0.04}
-            x={width * 0.10}
-            y={height * 0.79}
-            color={blue}
-            fontSize={40}
-            fontColor={white}
-            text={"C"}
-            fontWeight={800}
-            callback={null}
-        />
-          <Button
-            height={height * 0.14}
-            width={width * 0.04}
-            x={width * 0.10}
-            y={height * 0.88}
-            color={blue}
-            fontSize={40}
-            fontColor={white}
-            text={"D"}
-            fontWeight={800}
-            callback={null}
-        />
-        {/* Save Button */}
-        <RectButton
-          height={height * 0.13}
-          width={width * 0.26}
-          x={width * 0.58}
-          y={height * 0.93}
-          color={neonGreen}
-          fontSize={width * 0.014}
-          fontColor={white}
-          text={"SAVE DRAFT"}
-          fontWeight={800}
-          callback={ () => writeToDatabaseDraft() } // Implement Save feature
-        />
-        {/* Cancel Button */}
-        <RectButton
-          height={height * 0.13}
-          width={width * 0.26}
-          x={width * 0.71}
-          y={height * 0.93}
-          color={red}
-          fontSize={width * 0.015}
-          fontColor={white}
-          text={"CANCEL"}
-          fontWeight={800}
-          callback={mainCallback} // Exit Back To Home
-        />
-        {/* Publish Button */}
-        <RectButton
-          height={height * 0.13}
-          width={width * 0.26}
-          x={width * 0.45}
-          y={height * 0.93}
-          color={blue}
-          fontSize={width * 0.015}
-          fontColor={white}
-          text={"PUBLISH"}
-          fontWeight={800}
-          callback={ () => writeToDatabaseConjecture() } // publish to database
-        />
-        {/* Back Button */}
-        <Button
-          height={height * 0.32}
-          width={width * 0.07}
-          x={width * 0.06}
-          y={height * 0.15}
-          color={red}
-          fontSize={width * 0.015}
-          fontColor={white}
-          text={"BACK"}
-          fontWeight={800}
-          callback={mainCallback} // Exit Back To Home
-        />
-        {/* 'X' Buttons for the mutliple choice boxes */}
-        <InputBox
-          height={height * 0.14}
-          width={width * 0.07}
-          x={width * 0.735}
-          y={height * 0.58}
-          color={white}
-          fontSize={width * 0.024}  //  Dynamically modify font size based on screen width
-          fontColor={black}
-          text={localStorage.getItem("OptionA Checkmark")}
-          fontWeight={600}
-          callback={() => send("OPTIONA")}
-        />
-        <InputBox
-          height={height * 0.14}
-          width={width * 0.07}
-          x={width * 0.735}
-          y={height * 0.67}
-          color={white}
-          fontSize={width * 0.024}  //  Dynamically modify font size based on screen width
-          fontColor={black}
-          text={localStorage.getItem("OptionB Checkmark")}
-          fontWeight={600}
-          callback={() => send("OPTIONB")}
-        />
-        <InputBox
-          height={height * 0.14}
-          width={width * 0.07}
-          x={width * 0.735}
-          y={height * 0.76}
-          color={white}
-          fontSize={width * 0.024}  //  Dynamically modify font size based on screen width
-          fontColor={black}
-          text={localStorage.getItem("OptionC Checkmark")}
-          fontWeight={600}
-          callback={() => send("OPTIONC")}
-        />
-        <InputBox
-          height={height * 0.14}
-          width={width * 0.07}
-          x={width * 0.735}
-          y={height * 0.85}
-          color={white}
-          fontSize={width * 0.024}  //  Dynamically modify font size based on screen width
-          fontColor={black}
-          text={localStorage.getItem("OptionD Checkmark")}
-          fontWeight={600}
-          callback={() => send("OPTIOND")}
-        />
-      </>
-    );
-  };
+  return (
+    <>
+      <Background height={height * 1.1} width={width} />
+      <NameBox height={height} width={width} boxState={state.value} />
+      <PINBox height={height} width={width} />
+      <StartBox height={height * 0.5} width={width * 0.5} x={5} y={4.6} boxState={null} similarityScores={null} inCE={true} />
+      <IntermediateBox height={height * 0.5} width={width * 0.5} x={9} y={1.906} boxState={null} similarityScores={null} inCE={true} />
+      <EndBox height={height * 0.5} width={width * 0.5} x={13} y={1.2035} boxState={null} similarityScores={null} inCE={true} />
+      {/* Button to Pose Editor */}
+      <Button
+        height={height * 0.12}
+        width={width * 0.0950}
+        x={width * 0.17}
+        y={height * 0.42}
+        color={blue}
+        fontSize={21}
+        fontColor={white}
+        text={"POSE EDITOR"}
+        fontWeight={800}
+        callback={editCallback}
+      />
+      <Button
+        height={height * 0.14}
+        width={width * 0.04}
+        x={width * 0.10}
+        y={height * 0.61}
+        color={blue}
+        fontSize={40}
+        fontColor={white}
+        text={"A"}
+        fontWeight={800}
+        callback={null}
+      />
+      <Button
+        height={height * 0.14}
+        width={width * 0.04}
+        x={width * 0.10}
+        y={height * 0.70}
+        color={blue}
+        fontSize={40}
+        fontColor={white}
+        text={"B"}
+        fontWeight={800}
+        callback={null}
+      />
+      <Button
+        height={height * 0.14}
+        width={width * 0.04}
+        x={width * 0.10}
+        y={height * 0.79}
+        color={blue}
+        fontSize={40}
+        fontColor={white}
+        text={"C"}
+        fontWeight={800}
+        callback={null}
+      />
+      <Button
+        height={height * 0.14}
+        width={width * 0.04}
+        x={width * 0.10}
+        y={height * 0.88}
+        color={blue}
+        fontSize={40}
+        fontColor={white}
+        text={"D"}
+        fontWeight={800}
+        callback={null}
+      />
+      {/* Save Button */}
+      <RectButton
+        height={height * 0.13}
+        width={width * 0.26}
+        x={width * 0.58}
+        y={height * 0.93}
+        color={neonGreen}
+        fontSize={width * 0.014}
+        fontColor={white}
+        text={"SAVE DRAFT"}
+        fontWeight={800}
+        callback={ () =>{
+          writeToDatabaseConjecture();
+          setIsSaved(true);
+          
+        }}/>
+      {/* Cancel Button */}
+      <RectButton
+        height={height * 0.13}
+        width={width * 0.26}
+        x={width * 0.71}
+        y={height * 0.93}
+        color={red}
+        fontSize={width * 0.015}
+        fontColor={white}
+        text={"CANCEL"}
+        fontWeight={800}
+        callback={() => {
+          localStorage.clear();
+          mainCallback();
+        }}  // Exit Back To Home
+      />
+      {/* Publish Button */}
+      <RectButton
+        height={height * 0.13}
+        width={width * 0.26}
+        x={width * 0.45}
+        y={height * 0.93}
+        color={blue}
+        fontSize={width * 0.015}
+        fontColor={white}
+        text={"PUBLISH"}
+        fontWeight={800}
+        callback={ () =>{
+          localStorage.clear();
+          writeToDatabaseConjecture();
+          setIsSaved(true);
+        } // publish to database
+        }/>
+      {/* Back Button */}
+      <Button
+        height={height * 0.32}
+        width={width * 0.07}
+        x={width * 0.06}
+        y={height * 0.15}
+        color={red}
+        fontSize={width * 0.015}
+        fontColor={white}
+        text={"BACK"}
+        fontWeight={800}
+        callback={() => {
+          if (!currentConjecture.isSaved) {
+            // If data hasn't been saved
+            const confirmLeave = window.confirm("You didnt save your work. Are you sure you want to leave?");
+            if (confirmLeave) {
+              // if User confirmed, clear local storage and go back
+              localStorage.clear();
+              mainCallback();
+            }
+          } else {
+            // If it is saved, just go back
+            localStorage.clear();
+            mainCallback();
+          }
+        }}
+      />
+      {/* 'X' Buttons for the mutliple choice boxes */}
+      <InputBox
+        height={height * 0.14}
+        width={width * 0.07}
+        x={width * 0.735}
+        y={height * 0.58}
+        color={white}
+        fontSize={width * 0.024}  //  Dynamically modify font size based on screen width
+        fontColor={black}
+        text={localStorage.getItem("OptionA Checkmark")}
+        fontWeight={600}
+        callback={() => send("OPTIONA")}
+      />
+      <InputBox
+        height={height * 0.14}
+        width={width * 0.07}
+        x={width * 0.735}
+        y={height * 0.67}
+        color={white}
+        fontSize={width * 0.024}  //  Dynamically modify font size based on screen width
+        fontColor={black}
+        text={localStorage.getItem("OptionB Checkmark")}
+        fontWeight={600}
+        callback={() => send("OPTIONB")}
+      />
+      <InputBox
+        height={height * 0.14}
+        width={width * 0.07}
+        x={width * 0.735}
+        y={height * 0.76}
+        color={white}
+        fontSize={width * 0.024}  //  Dynamically modify font size based on screen width
+        fontColor={black}
+        text={localStorage.getItem("OptionC Checkmark")}
+        fontWeight={600}
+        callback={() => send("OPTIONC")}
+      />
+      <InputBox
+        height={height * 0.14}
+        width={width * 0.07}
+        x={width * 0.735}
+        y={height * 0.85}
+        color={white}
+        fontSize={width * 0.024}  //  Dynamically modify font size based on screen width
+        fontColor={black}
+        text={localStorage.getItem("OptionD Checkmark")}
+        fontWeight={600}
+        callback={() => send("OPTIOND")}
+      />
+    </>
+  );
+};
   
-  export default ConjectureModule;
+export default ConjectureModule;
