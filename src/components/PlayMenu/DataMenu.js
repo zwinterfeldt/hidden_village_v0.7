38a8707a369c9eeaf397c9cca5a47321b5666762
PidgeonBrained <Ventex240@gmail.com>
2024-10-25 10:36:49 -0500

    Update DataMenu.js

--- src/components/PlayMenu/DataMenu.js
+++ src/components/PlayMenu/DataMenu.js
@@ -6,7 +6,8 @@ import { useCallback } from "react";
 import { TextStyle } from "@pixi/text";
 import InputBox from "../InputBox";
 import { Input } from 'postcss';
-
+import { useEffect, useRef } from 'react';
+import { getUserEmailFromDatabase } from "../../firebase/userDatabase"
 
 const DataMenu = (props) => {
   const {
@@ -21,11 +22,13 @@ const DataMenu = (props) => {
   const innerRectHeight = menuHeight * 0.8; 
   const innerRectMargins = (menuWidth - innerRectWidth) / 2;
   const fieldTextMarginsFromInnerRect = menuHeight * 0.07;
-  const fieldTextMarginsFromEachOther = menuHeight * 0.13;
+  const fieldTextMarginsFromEachOther = menuHeight * 0.11;
   const fieldHeight = menuWidth * 0.028;
-  const inputBoxHeight = fieldHeight * 3;
+  const inputBoxHeight = fieldHeight * 2;
   const inputBoxTextSize = menuWidth * 0.02;
-  const distanceFromFieldTextToField = menuWidth * 0.5;
+  const distanceFromFieldTextToField = menuWidth * 0.4;
+  const checkButtonWidth = menuWidth * 0.07;
+  const checkButtonFont = menuWidth * 0.05;
   const fieldText = new TextStyle({
     align: "center",                    
     fontFamily: "Arial",                 
@@ -106,22 +109,23 @@ const DataMenu = (props) => {
           height={inputBoxHeight}
           width={(innerRectWidth - distanceFromFieldTextToField - fieldTextMarginsFromInnerRect * 2) / 0.4}
           x={x + innerRectMargins + fieldTextMarginsFromInnerRect + distanceFromFieldTextToField}
-          y={y + menuHeight - innerRectMargins - innerRectHeight + fieldTextMarginsFromInnerRect}
+          y={y + menuHeight - innerRectMargins - innerRectHeight + fieldTextMarginsFromInnerRect - (fieldHeight / 2)}
           color={white}
           fontSize={inputBoxTextSize}  //  Dynamically modify font size based on screen width
           fontColor={black}
-          text={"null"}
+          text={getUserEmailFromDatabase()}
           fontWeight={600}
         />
-        {/* <input
-        value={email}
-        onChange={handleEmailChange}
-        id="email"
-        className="login-input-input"
-        type="email"
-        autoComplete="on"
-        /> */}
-
+      {/* <InputBox
+        height={checkButtonWidth}
+        width={checkButtonWidth}
+        x={x + innerRectMargins + fieldTextMarginsFromInnerRect}
+        y={y + menuHeight - innerRectMargins - innerRectHeight + fieldTextMarginsFromInnerRect + fieldTextMarginsFromEachOther * 4}
+        color={white}
+        fontSize={checkButtonFont}
+        fontColor={black}
+        text={"X"}
+      /> */}
     </Container>
     )
 } else {
